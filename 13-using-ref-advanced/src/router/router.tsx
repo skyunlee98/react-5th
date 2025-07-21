ab16f6384caab30f0756a2d9fe4f8e6f8b7c23c1
skyunlee98 <skyunlee@naver.com>
2025-07-21 10:14:19 +0900

    250721: useRef vs useState까지의 내용

---
+++ 13-using-ref-advanced/src/router/router.tsx
@@ -0,0 +1,30 @@
+import Home from "@/pages/Home/Home";
+import Remember from "@/pages/Remember";
+
+
+
+
+
+export const routes = [
+  {
+    title:'리액트 애니메이션',
+    path:'/',
+    element: <Home />
+  },
+  {
+    title:'리-렌더없이 기억하기',
+    path:'/re-render',
+    element: <Remember />
+  },
+]
+
+
+
+
+
+
+
+
+
+
+
